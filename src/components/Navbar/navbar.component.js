1802c1e173971b55e02dda46bffa1dd57df884f3
Leslie Lopez <leslie2515lopez@gmail.com>
2024-07-09 00:03:58 -0600

    Added npm package and finished setting up the navbar component

---
+++ src/components/Navbar/navbar.component.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./navbar.styles.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar" id="sticky">
+      <div className="max-width">
+        <div className="logo">
+          Portfo<span>lio</span>
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#home" className="menu-btn">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="menu-btn">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#interests" className="menu-btn">
+              Interests
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="menu-btn">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="menu-btn">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#experiences" className="menu-btn">
+              Experiences
+            </a>
+          </li>
+          <li>
+            <a href="#courses" className="menu-btn">
+              Courses
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="menu-btn">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className="menu-btn">
+          <i className="fas fa-bars mobile-menu"></i>
+        </div>
+      </div>
+    </nav>
+  );
+}
