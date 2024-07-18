0f65fc4cd9e5478d0494684a355f16ef0cb5de7b
Leslie Lopez <leslie2515lopez@gmail.com>
2024-07-18 00:00:05 -0600

    Fixed About component

--- src/components/About/about.component.js
+++ src/components/About/about.component.js
@@ -4,30 +4,36 @@ import "../../App.css";
 import "./about.styles.css";
 
 export default function About() {
-    return (
-        <main class="about" id="about">
-            <div class="max-width">
-                <h2 class="title"><span>About Me</span></h2>
-                <div class="about-content">
-                    <div class="column left">
-                        <img src="/assets/images/profile.png" alt="profile image" loading="lazy" />
-                    </div>
-                    <div class="column right">
-                        <h3 class="text">I'm Leslie Lopez</h3>
-                        <p>
-                            Hello! I'm a passionate React frontend developer dedicated to mastering the art of creating seamless user
-                            experiences.
-                            Focused on becoming a full-stack React developer, I thrive on continuous learning and problem-solving. If
-                            you're looking
-                            for a dedicated and adaptable web developer with a strong emphasis on React, let's connect and create
-                            exceptional
-                            projects together!<br>
-                                <br>Let's embark on this exciting tech journey together!
-                                </p>
-                            </div>
-                    </div>
-                </div>
-                </div>
-        </main>
-    )
+  return (
+    <main className="about" id="about">
+      <div className="max-width">
+        <h2 className="title">
+          <span>About Me</span>
+        </h2>
+        <div className="about-content">
+          <div className="column left">
+            <img
+              src="/assets/images/profile.png"
+              alt="profile pic"
+              loading="lazy"
+            />
+          </div>
+          <div className="column right">
+            <h3 className="text">I'm Leslie Lopez</h3>
+            <p>
+              Hello! I'm a passionate React frontend developer dedicated to
+              mastering the art of creating seamless user experiences. Focused
+              on becoming a full-stack React developer, I thrive on continuous
+              learning and problem-solving. If you're looking for a dedicated
+              and adaptable web developer with a strong emphasis on React, let's
+              connect and create exceptional projects together!
+              <br />
+              <br />
+              Let's embark on this exciting tech journey together!
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
