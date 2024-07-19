4bdcd0e2256b3e67803bca701942cc2d68dc70bc
Leslie Lopez <leslie2515lopez@gmail.com>
2024-07-18 23:58:12 -0600

    Added interests component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/navbar.component";
 import Home from "./components/Home/home.component";
 import About from "./components/About/about.component";
+import Interests from "./components/Interests/interests.component";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <Home />
       <About />
+      <Interests />
     </div>
   );
 }
---
+++ src/components/Interests/interests.component.js
@@ -0,0 +1,67 @@
+import React from "react";
+import codingImg from "../../assets/images/coding.svg";
+import mediateImg from "../../assets/images/meditate.svg";
+import drawingImg from "../../assets/images/drawing.svg";
+
+import "../../App.css";
+import "./interests.styles.css";
+
+export default function Interests() {
+  return (
+    <section className="interests" id="interests">
+      <div className="max-width">
+        <h2 className="title">
+          <span>Interests</span>
+        </h2>
+        <div className="interests-content">
+          <div className="card">
+            <div className="box">
+              <img
+                className="interests-imgs"
+                src={codingImg}
+                alt="coding"
+                loading="lazy"
+              />
+              <h3 className="text">Coding</h3>
+              <p>
+                I have a lot of fun coding and building projects to keep
+                improving my skills.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="box">
+              <img
+                className="interests-imgs"
+                src={mediateImg}
+                alt="meditate"
+                loading="lazy"
+              />
+              <h3 className="text">Meditating</h3>
+              <p>
+                Meditation is my go-to practice for relaxation and stress
+                relief, especially when faced with challenging bugs or complex
+                coding puzzles.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="box">
+              <img
+                className="interests-imgs"
+                src={drawingImg}
+                alt="drawing"
+                loading="lazy"
+              />
+              <h3 className="text">Drawing</h3>
+              <p>
+                I have a creative side that I nurture through drawing and
+                graphic design.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
