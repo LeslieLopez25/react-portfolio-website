ff1ec961aedf9cbcd42dc4065b0d971579df4209
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-22 22:49:59 -0600

    Updated home component

--- src/components/Home/home.component.js
+++ src/components/Home/home.component.js
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="max-width">
         <div className="home-content">
           <p className="text-1">Hello, my name is</p>
-          <h1 className="text-2">Leslie Lopez</h1>
+          <h1 className="text-2">Leslie Lopez Anaya</h1>
           <p className="text-3">And I'm a Web Developer</p>
         </div>
       </div>
